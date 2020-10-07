683ab071a6268d1b6180cd0c795320e2f8582c9c
Jennifer St. Sume <jenniferst.sume@Jennifers-MacBook-Air.local>
2020-10-06 22:42:17 -0400

    Finalizing third-year draft.

--- Asian - 3YP.R
+++ Asian - 3YP.R
@@ -2,54 +2,57 @@
 #Multivariate model subet to only Asian 
 #######################################################################################
 amps <- cmps[complete.cases(cmps), ]
-amps <- filter(cmps, cmps$s2_4 == 1)
+amps <- subset(cmps, cmps$s2_4 == 1)
 
-#National identty index
-amps$c107 <- as.numeric(amps$c107)
-amps$c108 <- as.numeric(amps$c108)
-amps$c109 <- as.numeric(amps$c109)
-amps$c110 <- as.numeric(amps$c110)
-
-natid <-(rev(amps$c107) + rev(amps$c108) + amps$c109 + amps$c110)/4
-natid <- natid-1
-hist(natid)
-summary(natid)
-
+amps$natid <-(rev(amps$c107) + rev(amps$c108) + amps$c109 + amps$c110)/4
+amps$natid <- amps$natid-1
 #IVS
 amps$allrace <- amps$ethnic_quota
-allrace <- amps$allraces
+amps$allrace <- amps$allrace
+amps$Black <- amps$s2_3
+amps$White <- amps$s2_1
+amps$Latino <- amps$s2_2
+amps$Asian <- amps$s2_4
 
 #Controls
-income <-as.numeric(amps$c383)
-south <- amps$south
-citizen <-amps$citizen
-religion <- amps$c129
-age <- amps$age
-age <- cut_number(age,10)
-ed <- as.numeric(amps$c381)
-native <- amps$s7
-native <- ifelse(native == "United States", 1, 0) #Note that this does not include Puerto Rico. This is a separate category. Excluded because irrelevant to my theory. 
+amps$income <-as.numeric(amps$c383)
+amps$south <- amps$south
+amps$citizen <-amps$citizen
+amps$religion <- amps$c129
+amps$age <- as.numeric(amps$age)
+amps$ed <- as.numeric(amps$c381)
+amps$native <- amps$s7
+amps$native <- ifelse(amps$native == "United States", 1, 0) #Note that this does not include Puerto Rico. This is a separate category. Excluded because irrelevant to my theory. 
+amps$native <- as.numeric(amps$native)
 amps$party <- amps$c25 #3-point measure, secondary measure of intensity below  
 amps$party <- amps$party 
 amps$party <- as.numeric(amps$party)
 amps$party <- recode(amps$party,"c(3,4)= 0 ;c(1,2)= 1")
-party <- amps$party 
-amps$Dem <- as.numeric(amps$c25=="Democrat"); table(amps$Dem)
+amps$party <- amps$party 
+amps$Dem <- as.numeric(amps$c25=="Democrat")
 amps$partisan <- amps$c26
 amps$partisan <- fct_rev(amps$partisan)
-amps$partisan <- as.numeric(amps$partisan)-1
-lf <- as.numeric(amps$c150) -1;lf
-dlf <- as.numeric(fct_rev(amps$c151))
-female <- amps$s3
-female <- as.numeric(female)
-female <- ifelse(female == "Male", 0, 1)
+amps$lf <- as.numeric(amps$c150) -1
+amps$c.dlf <- fct_rev(amps$c151)
+amps$female <- amps$s3
+amps$female <- as.numeric(ifelse(amps$female == "Male", 0, 1))
+amps$female <- as.numeric(amps$female)
 
-sAsian <-glm(natid ~ citizen + female + native + south + income + lf + partisan + ed + age, 
-             family="gaussian", data=amps); summary(sAsian)
 
-sdAsian <-glm(natid ~ citizen + female + native + south + income + dlf + partisan + ed + age, 
-              family="gaussian", data=amps); summary(sdAsian)
-log(exp(coef(sdAsian)))
+#Modeling
+asian <-lm(natid ~ citizen + female + native + south + income + partisan + ed + age, data=amps)
 
-plotreg(sAsian, omit.coef = "Intercept")
-plotreg(sdAsian, omit.coef = "Intercept")
+sasian <-lm(natid ~ citizen + female + native + south + income + lf + partisan + ed + age, data=amps)
+
+sdasian <-lm(natid ~ citizen + female + native + south + income + c.dlf + partisan + ed + age, data=amps)
+#Descriptive Statistics
+ggbcoefnames <- c("National Identity", "Citizen", "Female", "Native Born", "South",
+                  "Income", "Linked Fate", "Partisanship", "Education", "Age")
+
+
+stargazer(amps[c("natid", "citizen", "female", "native", "south", 
+                 "income", "lf", "partisan", "ed", "age")] ,title = c("Descriptive Statistics"), covariate.labels = ggbcoefnames, digits = 2, type = "latex", header=FALSE)
+plotreg(asian, omit.coef = "Intercept")
+plotreg(sasian, omit.coef = "Intercept")
+plotreg(sdasian, omit.coef = "Intercept")
+plotreg(black, omit.coef = "Intercept")
